10ba9e8ac226ed8d7c880011c8f83e0c57ad705f
prestonGarvey1 <125075490+prestonGarvey1@users.noreply.github.com>
2026-02-04 00:27:52 -0500

    Add DriverCamera class for managing PhotonCamera functionality

---
+++ src/main/java/frc/robot/DriverCamera.java
@@ -0,0 +1,30 @@
+import org.photonvision.*;
+import org.photonvision.targeting.PhotonPipelineResult;
+public class DriverCamera {
+    PhotonCamera camera = new PhotonCamera("photonvision");
+    int index = 0;
+    
+    // Set pipeline index
+    public void setPipelineIndex(int index) {
+        camera.setPipelineIndex(index);
+    }
+
+    // set FPS limit to 60
+    public void setFPSLimit() {
+        camera.setFPSLimit(60);
+        /* Set FPS limit to -1 for unlimited FPS
+        camera.setFPSLimit(-1);
+        */
+    }
+
+    // Set driver mode off/on
+    public void toggleDriverMode() {
+        camera.setDriverMode(true);
+    }
+
+   
+    
+
+    
+    
+}
