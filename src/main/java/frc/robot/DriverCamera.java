d8bfbe518c22cb2d0cb4fc6be93c4e3f7002c760
prestonGarvey1 <Thebozzkid2010@gmail.com>
2026-03-17 08:29:45 -0400

    Fix format

--- src/main/java/frc/robot/DriverCamera.java
+++ src/main/java/frc/robot/DriverCamera.java
@@ -1,27 +1,24 @@
 package frc.robot;
 
 import org.photonvision.*;
+
+// This class contains the configuration settings for the driver camera (PhotonVision), they are set through networktables.
 public final class DriverCamera {
-    PhotonCamera camera1 = new PhotonCamera("Camera 1");
-    PhotonCamera camera2 = new PhotonCamera("Camera 2");
+    PhotonCamera camera1 = new PhotonCamera("OV9281");
+    PhotonCamera camera2 = new PhotonCamera("OV9281 (1)");
 
     // Driver camera settings
 
     //Camera 1 pipeline index
     int index1 = 1;
-
     //Camera 2 pipeline index
     int index2 = 2;
-
     //Set FPS limit to -1 for unlimited FPS
     int FPS = 48;
-
     //Toggle driver mode on/off.
     boolean toggleDriverMode = true;
 
-
-
-    
+    //Methods to encorporate the above settings, called in Robot.java to set the driver camera settings on rio startup.
     public DriverCamera() {
         setFPSLimit(FPS);
         toggleDriverMode(toggleDriverMode);
